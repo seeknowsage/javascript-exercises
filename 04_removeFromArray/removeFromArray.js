faec576cd5b1f383a24614b32402fdbb899cb8c5
seeknowsage <29027488+seeknowsage@users.noreply.github.com>
2023-09-26 06:59:05 -0500

    Fundamentals_Part_4_Note_Taking_092623

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
 
+    const originalArray = arguments[0];
+
+    let elementsToRemove = [];
+
+    for (let j = 1; j <= arguments.length - 1; j++) {
+        elementsToRemove.push(arguments[j]);
+    }
+
+    const finalArray = originalArray.filter(element => !elementsToRemove.includes(element)); 
+    //Checks if each element in originalArray is NOT included in the elementsToRemove array. 
+    //If it isn't, the condition evaluates to true, and the element is included in the finalArray. 
+    //If an element is included in elementsToRemove, the condition evaluates to false, and that element is filtered out.
+
+    return finalArray;
+    
 };
 
 // Do not edit below this line
